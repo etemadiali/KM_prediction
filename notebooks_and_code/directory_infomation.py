4cba3f9c0ec4537749e509fc77862a6fc9074ec9
Ali Etemadi <135605381+etemadism@users.noreply.github.com>
2023-06-21 13:45:41 +0000

    new file:   datasets/BRENDA_data/brenda_df_checkpoint_3_V2_py3.7.pkl 	modified:   datasets/enzyme_data/all_sequences.fasta 	new file:   datasets/enzyme_data/test.fasta 	modified:   notebooks_and_code/Downloading and preprocessing BRENDA data.ipynb 	modified:   notebooks_and_code/directory_infomation.py

--- notebooks_and_code/directory_infomation.py
+++ notebooks_and_code/directory_infomation.py
@@ -1,9 +1,9 @@
 #specify the directory that contains the folder with all datsets:
-datasets_dir = "C:\\Users\\alexk\\projects\\Prediction_of_KM_V3\\datasets\\"
+datasets_dir = "/workspaces/KM_prediction/datasets/"
 
 brenda_email_address = ""
 brenda_password = ""
 
 #specifying path with additional code:
 import sys
-sys.path.append('.\\additional_code')
+sys.path.append('./additional_code/')
